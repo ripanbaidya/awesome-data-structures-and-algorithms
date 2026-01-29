e2cfea9a7b2ad49a8c38218d094a301b96ece419
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-01-29 20:43:08 +0530

    move zeros

--- data-structures/java/src/array/easy/MoveZeros.java
+++ data-structures/java/src/array/easy/MoveZeros.java
@@ -2,82 +2,79 @@ package array.easy;
 
 import java.util.Arrays;
 
-/**
+/*
  * @author Ripan Baidya
- * @date 30-07-2025
+ * @date 29-01-2026
  *
  * Given an integer array nums, move all 0's to the end of it while maintaining the relative order
  * of the non-zero elements. Note that you must do this in-place without making a copy of the array.
- *
  * Example:
- *
  * Input: nums = [0,1,0,3,12]
  * Output: [1,3,12,0,0]
  */
 public class MoveZeros {
-    /**
-     * The idea is to move all the zeros to the end of the array while maintaining the relative order
-     * of non-zero elements using two traversals.
+
+    /* ---------------- Better 1: Two Pointers - Two Pass ---------------- */
+
+    /*
+     * The idea is to move all the zeros to the end of the array while maintaining the relative
+     * order of non-zero elements using two traversals.
      *
      * Time Complexity: O(n), as we are traversing the array only twice.
      * Auxiliary Space: O(1)
      */
-    /*
-    public void moveZeroes(int[] nums) {
-        int n = nums.length; // array length
-        int count = 0; // count of non-zero elements
+    public void moveZeroesBetter1(int[] nums) {
+        int n = nums.length;
+        int count = 0; // count non-zero elements
 
         // If the current element is not zero, place it at the 'count' position
         // and move the 'count' pointer to the next position.
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
-                nums[count ++] = nums[i];
+                nums[count++] = nums[i];
             }
         }
 
         // Now all non-zero elements have been shifted to
         // the front. Make all elements 0 from count to end.
         while (count < n) {
-            nums[count ++] = 0;
+            nums[count++] = 0;
         }
     }
-    */
 
-    /**
-     * The idea is similar to the previous approach where we took a pointer, say count to track where the next
-     * non-zero element should be placed. However, on encountering a non-zero element, instead of directly
-     * placing the non-zero element at arr[count], we will swap the non-zero element with arr[count]. This
-     * will ensure that if there is any zero present at arr[count], it is pushed towards the end of array
-     * and is not overwritten.
+    /* ---------------- Better 2: Two Pointer - One Pass ---------------- */
+
+    /*
+     * We can fine-tune our previous two-pointer approach by swapping in place.
+     * Maintain two indices:
+     * - writePos points to the next position where a non-zero should live.
+     * - readPos scans the array left to right.
+     * Whenever nums[readPos] is non-zero, swap it with nums[writePos]
+     * (only if readPos != writePos) and advance writePos. This compacts non-zeros
+     * in a single pass and leaves zeros behind naturally.
      *
-     * Time Complexity: O(n), as we are traversing the array only once.
-     * Space Complexity: O(1)
+     * Time Complexity: O(n), as the array is traversed once.
+     * Space Complexity: O(1), no additional data structures are used.
      */
-    public void moveZeroes(int[] nums) {
-        int n = nums.length; // array length
-        // Pointer to track the position for next non-zero element
-        int count = 0;
-
-        for (int i = 0; i < n; i ++) {
-            // when current element is non-zero
-            if (nums[i] != 0) {
-                // Swap the current element with the 0 at index 'count'
-                int temp = nums[count];
-                nums[count] = nums[i];
-                nums[i] = temp;
-
-                // move count pointer to next position
-                count ++;
+    public void moveZeroesBetter2(int[] nums) {
+        int writePos = 0; // next slot for a non-zero
+        for (int readPos = 0; readPos < nums.length; readPos++) {
+            if (nums[readPos] != 0) {
+                if (readPos != writePos) {
+                    int tmp = nums[readPos];
+                    nums[readPos] = nums[writePos];
+                    nums[writePos] = tmp;
+                }
+                writePos++;
             }
         }
     }
 
     public static void main(String[] args) {
-        var obj = new MoveZeros();
+        var solution = new MoveZeros();
+        
+        int nums[] = {0, 1, 0, 3, 12};
 
-        int[] nums = {0, 1, 0, 3, 12};
-        obj.moveZeroes(nums);
-
-        System.out.println(Arrays.toString(nums));
+        solution.moveZeroesBetter1(nums);
     }
 }
