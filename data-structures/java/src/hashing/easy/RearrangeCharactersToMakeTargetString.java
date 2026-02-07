a0aa1eab87c35cdc3be42f333529555fbcb2b711
Ripan Baidya <ripanbaidya7980@gmail.com>
2026-02-07 12:56:57 +0530

    feat:  imporve code

--- data-structures/java/src/hashing/easy/RearrangeCharactersToMakeTargetString.java
+++ data-structures/java/src/hashing/easy/RearrangeCharactersToMakeTargetString.java
@@ -9,59 +9,78 @@ import java.util.Map;
  */
 public class RearrangeCharactersToMakeTargetString {
 
-    /* ---------------- Better ---------------- */
+  /* ---------------- Better ---------------- */
 
-    public int rearrangeCharactersBetter(String s, String target) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
-
-        Map<Character, Integer> freqTarget = new HashMap<>();
-        for (char c : target.toCharArray()) {
-            freqTarget.put(c, freqTarget.getOrDefault(c, 0) + 1);
-        }
-
-        int result = Integer.MAX_VALUE;
-        for (Map.Entry<Character, Integer> entry : freqTarget.entrySet()) {
-            char ch = entry.getKey();
-            int needed = entry.getValue();
-            result = Math.min(result, freq[ch - 'a'] / needed);
-        }
-
-        return result;
+  /**
+   * Time Complexity: O(n)
+   * Space Complexity: O(n)
+   */
+  public int rearrangeCharactersBetter(String s, String target) {
+    int[] characterFreq = new int[26];
+    // Count the frequency of each character of String 's'
+    for (char c : s.toCharArray()) {
+      characterFreq[c - 'a']++;
     }
 
-    /* ---------------- Optimal ---------------- */
-
-    public int rearrangeCharacters(String s, String target) {
-        int[] freq = new int[26];
-        int[] freq2 = new int[26];
-
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
-
-        for (char c : target.toCharArray()) {
-            freq2[c - 'a']++;
-        }
-
-        int result = Integer.MAX_VALUE;
-        for (int i = 0; i < 26; i++) {
-            if (freq2[i] > 0) {
-                result = Math.min(result, freq[97 + i - 'a'] / freq2[i]);
-            }
-        }
-
-        return result;
+    // Count the frequency of each character of String 'target'
+    Map<Character, Integer> targetFreq = new HashMap<>();
+    for (char c : target.toCharArray()) {
+      targetFreq.put(c, targetFreq.getOrDefault(c, 0) + 1);
     }
 
-    // Main
-    public static void main(String[] args) {
-        var solution = new RearrangeCharactersToMakeTargetString();
-
-        String s = "ilovecodingonleetcode", target = "code";
-        int result = solution.rearrangeCharacters(s, target);
-        System.out.println(result);
+    int result = Integer.MAX_VALUE;
+    for (Map.Entry<Character, Integer> entry : targetFreq.entrySet()) {
+      char ch = entry.getKey();
+      int needed = entry.getValue();
+      result = Math.min(result, characterFreq[ch - 'a'] / needed);
     }
+
+    return result;
+  }
+
+  /* ---------------- Optimal ---------------- */
+
+  /**
+   * Time Complexity: O(n)
+   * Space Complexity: O(1)
+   */
+  public int rearrangeCharacters(String s, String target) {
+    int[] characterFreq = new int[26];
+    int[] targetFreq = new int[26];
+
+    // Count the frequency of each character of String 's'
+    for (char c : s.toCharArray()) {
+      characterFreq[c - 'a']++;
+    }
+    // Count the frequency of each character of String 'target'
+    for (char c : target.toCharArray()) {
+      targetFreq[c - 'a']++;
+    }
+
+    // Initialize with 0, as we are calculating the maximum
+    int result = 0;
+
+    // Since, there are 26 possible characters, we are interested only in those characters
+    // which are present in the target string, so we will only consider those characters and
+    // calculate the maximum number of target string that can be formed
+    for (int i = 0; i < 26; i++) {
+      if (targetFreq[i] > 0) {
+        int possibleTarget = characterFreq[i] / targetFreq[i];
+        result = Math.min(result, possibleTarget);
+      }
+    }
+
+    return result;
+  }
+
+  // Main
+  public static void main(String[] args) {
+    var solution = new RearrangeCharactersToMakeTargetString();
+
+    String s = "ilovecodingonleetcode";
+    String target = "code";
+    
+    int result = solution.rearrangeCharacters(s, target);
+    System.out.println(result);
+  }
 }
